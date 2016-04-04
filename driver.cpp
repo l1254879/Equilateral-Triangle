da115fe739373185697430b1dc7292a5e2e2b5f1
l1254879 <l1254879@gmail.com>
2016-04-05 00:37:48 +0800

    add some limit and deal with Draw function

--- driver.cpp
+++ driver.cpp
@@ -10,6 +10,7 @@ int main(){
 	cout<<"\nt2 has size = "<<t2.GetSize()<<"units.\n";
 	cout<<"\nt3 has size = "<<t3.GetSize()<<"units.\n";
 	cout<<"\nt4 has size = "<<t4.GetSize()<<"units.\n";
+	t1.Draw();
 
 
 
--- triangle.cpp
+++ triangle.cpp
@@ -7,10 +7,13 @@ using namespace std;
 
 triangle::triangle(int call_size){
 	size = call_size;
+	border = '#';
+	fill = '*';
 }
 triangle::triangle(int call_size,const char call_border){
 	size = call_size;
 	border = call_border;
+	fill = '*';
 }
 triangle::triangle(int call_size,const char call_border,const char call_fill){
 	size = call_size;
@@ -18,7 +21,12 @@ triangle::triangle(int call_size,const char call_border,const char call_fill){
 	fill = call_fill;
 }
 int triangle::GetSize(){
-	return size;
+	if(size > 39)
+		return 39;
+	else if(size < 1)
+		return 1;
+	else 
+		return size;
 }
 int triangle::Perimeter(){	
 	return 3*size;
@@ -27,14 +35,39 @@ double triangle::Area(){
 	return size*(size*0.5*sqrt(3));
 }
 int triangle::Grow(){
-	return size + 1;
+	if(size > 39){
+		cout<<"size cannot be more than 39"<<endl;
+		return 39;
+	}
+	else
+		return size + 1;
 }
 int triangle::Shrink(){
-	return size - 1;
+	if(size < 1){
+		cout<<"size cannot be less than 1"<<endl;
+		return 1;
+	}
+	else
+		return size - 1;
 }
 void triangle::Summary(){
 	cout<<"Size of triangle's side = "<<size<<" units.\n";
 	cout<<"Perimeter of triangle = "<<Perimeter()<<" units.\n";
 	cout<<"Area of triangle = "<<Area()<<" units.\n";
 	cout<<"triangle looks like : "<<endl;
+}
+void triangle::Draw(){
+	for(int i=0;i<=size;i++){
+		for(int k=size-1;k>=i;k--)
+			cout<<" ";
+		for(int j=0;j<i;j++){
+	
+			if(j==0||j==i-1||i==size)
+				cout<<border<<' ';
+			else
+				cout<<fill<<' ';
+			
+		}
+		cout<<"\n";
+	}
 }
