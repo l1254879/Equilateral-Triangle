1cc348401178328e257ff17ef395b2f50f988b90
l1254879 <l1254879@gmail.com>
2016-04-08 23:46:22 +0800

    add the comment in triangle.cpp

--- triangle.cpp
+++ triangle.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+//Judge the size
 int triangle::JudgeSize(int call_size){
 	if(call_size > 39)
 		size = 39;
@@ -13,6 +14,7 @@ int triangle::JudgeSize(int call_size){
 	else 
 		size = call_size;
 }
+//Judge the border
 char triangle::JudgeBorder(char call_border){
 	int trns_ascii;
 	trns_ascii = call_border;
@@ -21,6 +23,7 @@ char triangle::JudgeBorder(char call_border){
 	else
 		border = '#';
 }
+//Judge the fill
 char triangle::JudgeFill(char call_fill){
 	int trns_ascii;
 	trns_ascii = call_fill;
@@ -29,73 +32,73 @@ char triangle::JudgeFill(char call_fill){
 	else
 		fill = '*';
 }
+//one parameter in function
 triangle::triangle(int call_size){
 	JudgeSize(call_size);
 	border = '#';
 	fill = '*';
 }
+//two parameter in function
 triangle::triangle(int call_size,const char call_border){
 	JudgeSize(call_size);
 	JudgeBorder(call_border);
 	fill = '*';
 }
+//three parameter in function
 triangle::triangle(int call_size,const char call_border,const char call_fill){
 	JudgeSize(call_size);
 	JudgeBorder(call_border);
 	JudgeFill(call_fill);
 }
+//return the size
 int triangle::GetSize(){
 	return size;
 }
+//return the perimeter
 int triangle::Perimeter(){	
-	return 3*size;
+	return 3*size;//triangle have three side 
 }
+//return the area
 double triangle::Area(){
-	return size*(size*0.5*sqrt(3))*0.5;
+	return size*(size*0.5*sqrt(3))*0.5;//triangle formula is side*high/2
 }
+//size add 1
 int triangle::Grow(){
-	if(size+1 <= 39){
+	if(size+1 <= 39){// size add 1 cannot over the 39
 		size = size + 1;
 		return size;
 	}
 	else
 		return 39;
 }
+//size minus 1
 int triangle::Shrink(){
-	if(size-1 >= 1){
+	if(size-1 >= 1){// size minus one cannot less than 1 
 		size = size - 1;
 		return size;
 	}
 	else
 		return 1;
 }
+//renew the border
 void triangle::SetBorder(char set_border){
-	/*int trns_ascii;
-	trns_ascii = set_border;
-	if(trns_ascii >= 33 || trns_ascii <= 126)
-		border = set_border;
-	else
-		cout<<"Border cannot set"<<endl;
-		*/
+	
 	JudgeBorder(set_border);
 }
+//renew the fill
 void triangle::SetFill(char set_fill){
-	/*int trns_ascii;
-	trns_ascii = set_fill;
-	if(trns_ascii >= 33 || trns_ascii <= 126)
-		fill = set_fill;
-	else
-		cout<<"Border cannot set"<<endl;
-		*/
 	JudgeFill(set_fill);
 }
+//show all the info
 void triangle::Summary(){
 	cout<<"Size of triangle's side = "<<size<<" units.\n";
 	cout<<"Perimeter of triangle = "<<Perimeter()<<" units.\n";
 	cout<<"Area of triangle = "<<Area()<<" units.\n";
 	cout<<"triangle looks like : "<<endl;
 	Draw();
+	cout<<endl;
 }
+//draw the triangle
 void triangle::Draw(){
 	for(int i=0;i<=size;i++){
 		for(int k=size;k>=i;k--)
